d794d10bf7b7dac4aafa8038b5d5c81dcec429ab
Vova <vova@ekreative.com>
2017-08-02 09:22:13 +0300

    init

---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== WP Ek Health Check ===
+Contributors: ekreative
+Tags: healthcheck
+Requires at least: 2.7
+Tested up to: 4.8
+Stable tag: 1.0.0
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Check if site and database is up and return wordpress version
+
+== Description ==
+
+You can use url <your domain name>/healthcheck to check if your site is up and get current wordpress version of your site
+
+== Installation ==
+
+1. Download
+2. Upload to your `/wp-contents/plugins/` directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Changelog ==
---
+++ wp-ek-health-check.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: WP Ek Health Check
+Description: A plugin returns version of WP.
+Version: 1.0
+Author: eKreative
+Author URI:  https://www.ekreative.com
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+add_action( 'parse_request', 'wpversion_parse_request' );
+function wpversion_parse_request( &$wp )
+{
+    if ( in_array( 'healthcheck', $wp->query_vars ) ) {
+        global $wpdb;
+        header("Cache-Control: no-cache");
+        header("Content-type: application/json");
+        echo json_encode(['app' => true, 'database' => $wpdb->check_connection(), 'version' => get_bloginfo( 'version'), 'framework' => 'Wordpress']);
+        exit();
+    }
+    return;
+}
